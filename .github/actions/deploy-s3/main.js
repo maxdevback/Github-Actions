f545dca5e2b2915f061d951ee89e6fe9fc1b27e3
maxdevback <zacharchuk.max.dev@gmail.com>
2023-10-16 19:17:54 +0300

    Added deploy

---
+++ .github/actions/deploy-s3/main.js
@@ -0,0 +1,16 @@
+const core = require("@actions/core");
+const exec = require("@actions/exec");
+
+function run() {
+  const bucket = core.getInput("bucket", { required: true });
+  const bucketRegion = core.getInput("bucket-region", { required: true });
+  const distFolder = core.getInput("dist-folder", { required: true });
+
+  const s3Uri = `s3://${bucket}`;
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
+  const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+  core.setOutput("website-url", websiteUrl); // ::set-output
+}
+
+run();
